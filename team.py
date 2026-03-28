7dcb6f621658aebe3bbe7037d4eb4dff17c139df
Max Seeberg <maxse@ulrik.uio.no>
2026-03-28 13:11:04 +1100

    Added team.py file

---
+++ team.py
@@ -0,0 +1,29 @@
+# COMP30024 Artificial Intelligence, Semester 1 2026
+# Project Partner Nomination
+
+# Fill in your team details here. Be sure to keep a copy of this file for use in
+# future project submissions (this is how we identify your team).
+
+metadata = {
+    # Choose a team name! Try to make it unique in some way, as it will be
+    # used to identify your team in the tournament at the end of the semester.
+    # - Allowed characters: A-Z, a-z, 0-9, -, _ (no spaces allowed)
+    # - Minimum length: 3 characters
+    # - Maximum length: 20 characters
+    "team_name": f"Arctic_Chili", 
+
+    # Fill in the student IDs and emails of both team members here. There must
+    # be exactly two members in the list. 
+    "team_members": [
+        {
+            "name": "Max Seeberg",
+            "student_id": "1845732",
+            "email": "max.seeberg@student.unimelb.edu.au" # Must be @student.unimelb.edu.au address
+        },
+        {
+            "name": "James Leelayuvat",
+            "student_id": "1829212",
+            "email": "james.leelayuvat@student.unimelb.edu.au" # Must be @student.unimelb.edu.au address
+        }
+    ],
+}
